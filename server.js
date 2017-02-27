0be4e5b954db8664786073db71f0d65471b750e6
Orlando Vallejos <orlandovallejos@gmail.com>
2017-02-26 22:36:08 -0300

    Usernames working Now I added usernames for a better chat experience. This is now sending more messages and receiving signals. I learnt how to add properties to the socket object in server.

--- server.js
+++ server.js
@@ -23,12 +23,36 @@ io.sockets.on('connection', function (socket) {
     console.log('Connected: Sockets connected: %s', connections.length);
 
     socket.on('disconnect', function (data) {
+        //remove the user from list:
+        users.splice(users.indexOf(data), 1);
+
+        //remove the socket from list:
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: Sockets connected: %s', connections.length);
+
+        sendUsernames();
     });
 
     socket.on('send message', function (data) {
+        console.log('Server send message: ');
         console.log(data);
-        io.sockets.emit('new message', { msg: data });
+        io.sockets.emit('new message', { msg: data, user: socket.username });
     });
+
+    socket.on('login', function (data, callback) {
+        callback(true);
+
+        //here I add a property to the socket:
+        socket.username = data;
+
+        console.log('Server login:');
+        console.log(data);
+        users.push(data);
+
+        sendUsernames();
+    });
+
+    function sendUsernames() {
+        io.sockets.emit('get users', users);
+    }
 });
